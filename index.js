e16c9b5702e83235ecc1f175ffc9e9242d51e036
Cintia Vazquez <vazquezalvarezcintia@gmail.com>
2022-04-05 23:19:07 +0200

    Add dynamic array question log

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import Bookmark from "./components/bookmark/bookmark.js";
 import Buttons from "./components/buttons/buttons.js";
 import Singlepage from "./singlepage.js";
+import turnLogintoCard from "./question-post/question-card.js";
+
+// Single Page App
+Singlepage();
+
+//Create cards
+turnLogintoCard();
 
 // bookmark function
 const bookmarks = document.querySelectorAll(".js-question__bookmark");
@@ -15,6 +22,3 @@ const questionwrap = document.querySelectorAll(".js-questionwrap");
 questionwrap.forEach((questionElement) => {
   Buttons(questionElement);
 });
-
-// Single Page App
-Singlepage();
