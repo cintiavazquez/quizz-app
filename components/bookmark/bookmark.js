0d60bdaecfc1e501bbef605c95afa4fb97a0a7cb
Cintia Vazquez <vazquezalvarezcintia@gmail.com>
2022-04-07 16:00:54 +0200

    Add linting

--- components/bookmark/bookmark.js
+++ components/bookmark/bookmark.js
@@ -1,14 +1,14 @@
 export default function Bookmark(bookmarkElement) {
-  const bookmarkempty = bookmarkElement.querySelector(".js-bookmarkempty");
-  const bookmarkfilled = bookmarkElement.querySelector(".js-bookmarkfilled");
+  const bookmarkempty = bookmarkElement.querySelector('.js-bookmarkempty');
+  const bookmarkfilled = bookmarkElement.querySelector('.js-bookmarkfilled');
 
-  bookmarkempty.addEventListener("click", () => {
-    bookmarkempty.classList.toggle("display-none");
-    bookmarkfilled.classList.toggle("display-none");
+  bookmarkempty.addEventListener('click', () => {
+    bookmarkempty.classList.toggle('display-none');
+    bookmarkfilled.classList.toggle('display-none');
   });
 
-  bookmarkfilled.addEventListener("click", () => {
-    bookmarkfilled.classList.toggle("display-none");
-    bookmarkempty.classList.toggle("display-none");
+  bookmarkfilled.addEventListener('click', () => {
+    bookmarkfilled.classList.toggle('display-none');
+    bookmarkempty.classList.toggle('display-none');
   });
 }
